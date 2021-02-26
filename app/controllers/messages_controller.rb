98250e75dd029184eaddd52fd32070f18d500980
yonblog <77332211+yonblog@users.noreply.github.com>
2021-02-26 11:23:34 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,17 +5,23 @@ class MessagesController < ApplicationController
     # ↑「Messageモデルのからのインスタンス（Message.new）」
     @room = Room.find(params[:room_id])
     # ↑「指定のチャットルーム(今回はroomテーブルのid)のレコード情報」
+    @messages = @room.messages.includes(:user)
+    # ↑モデル名.includes(:アソシエーションで定義した関連名) 
+    # ↑ルーム名.ルームに関連するメッセージ全部.includes(:投稿したしたユーザー) → Nobuのチャットルーム.その中のメッセージ.の中でnobuが発信したメッセージ
+    # ※「includes」を使えば①roomテーブルの情報を取得 ②それに関連するmessageテーブルの情報を取得 と2回のアクセスで済むためN+1問題が解決される。
   end
-
+  
   def create
     @room = Room.find(params[:room_id])
     @message = @room.messages.new(message_params)
-      # ↑チャットルームに紐付いたメッセージのインスタンスを生成している。※ルーティングのネストをしているからできる。
+    # ↑チャットルームに紐付いたメッセージのインスタンスを生成している。※ルーティングのネストをしているからできる。
     if @message.save
       # ↑生成したインスタンスを@messageに代入し、saveメソッドでメッセージの内容をmessagesテーブルに保存している。
       redirect_to room_messages_path(@room)
       # ↑「もしメッセージが保存できたら、情報を更新してmessages#indexに遷移する。」
     else
+      @messages = @room.messages.includes(:user)
+      # ↑renderを用いることで、投稿に失敗した@messageの情報を保持しつつindex.html.erbを参照することができるがこの時、indexアクションは経由しないため@messagesが定義されていないとエラーになってしまうから。
       render :index
       # ↑「保存できなかったら、情報を更新せずにmessages#indexに遷移する。」
     end
