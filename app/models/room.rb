b54a073c2dbd951d4616809658bf26f079f00033
yonblog <77332211+yonblog@users.noreply.github.com>
2021-02-26 13:56:05 +0900

    チャットルームの削除機能の実装

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,7 +1,9 @@
 class Room < ApplicationRecord
-  has_many :room_users
+  has_many :room_users, dependent: :destroy
+                         # ↑dependentオプションに:destroyを指定したときは、親モデルが削除されたとき、それに紐付ている子モデルも一緒に削除される。
   has_many :users, through: :room_users
-  has_many :messages
+  has_many :messages, dependent: :destroy
+                         # ↑dependentオプションに:destroyを指定したときは、親モデルが削除されたとき、それに紐付ている子モデルも一緒に削除される。
 
   validates :name, presence: true
 end
