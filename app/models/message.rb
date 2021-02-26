61a02213fbd5fde63fb36e971ecd3577bc70b2ea
yonblog <77332211+yonblog@users.noreply.github.com>
2021-02-26 14:36:07 +0900

    画像表示の実装

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,7 @@ class Message < ApplicationRecord
   belongs_to :room
   belongs_to :user
   has_one_attached :image
-    # ↑各レコードとファイルを1対1の関係で紐づけるメソッド。
+    # ↑各レコードとファイルを1対1の関係で紐づけるメソッド。「has_one_attached :任意のファイル名」
     # ※「モデル.ファイル名」で、添付されたファイルにアクセスできるようになり、このファイル名は、そのモデルが紐づいたフォームから送られるパラメーターのキーにもなる。
 
   validates :content, presence: true
