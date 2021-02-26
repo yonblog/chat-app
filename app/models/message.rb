0fabcfd87c55d459b9d67cd48cf758e6aa7fdbfb
yonblog <77332211+yonblog@users.noreply.github.com>
2021-02-26 14:17:56 +0900

    Active Storageの導入

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,9 @@
 class Message < ApplicationRecord
   belongs_to :room
   belongs_to :user
+  has_one_attached :image
+    # ↑各レコードとファイルを1対1の関係で紐づけるメソッド。
+    # ※「モデル.ファイル名」で、添付されたファイルにアクセスできるようになり、このファイル名は、そのモデルが紐づいたフォームから送られるパラメーターのキーにもなる。
 
   validates :content, presence: true
 end
